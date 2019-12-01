987e1badbd0460b81fa756d2177d456ad4f42477
Aqudi <gjdigj@naver.com>
2019-12-01 12:44:06 +0900

    email 관련 문구, 인증 완료 화면 수정

--- api_server/models.py
+++ api_server/models.py
@@ -16,9 +16,16 @@ class Offer(models.Model):
         format='JPEG',
         options={'quality':60}
     )
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name="offers", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     open_kakao_link = models.URLField()
+
+    class Meta:
+        unique_together = ["id", "owner"]
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.title
